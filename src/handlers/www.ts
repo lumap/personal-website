738b2023077703c49cf25ce8403e6262eb94d41a
Hugo C <lumap@duck.com>
2023-09-26 22:18:49 +0200

    ok

--- src/handlers/www.ts
+++ src/handlers/www.ts
@@ -7,7 +7,6 @@ import { supportedLanguagesList } from "../consts/supportedLanguagesList";
 import { getCookie } from "../utils/serverCookies";
 import crypto from "crypto";
 import { exec } from "child_process";
-import { convertStreamToBinaryLike } from "../utils/stream2BinLike";
 
 export async function handleWWW(req: Request, route: string): Promise<Response> {
     let res: ResponseInit = {
@@ -30,23 +29,30 @@ export async function handleWWW(req: Request, route: string): Promise<Response> 
         }
         case "github-webhook": {
             const expectedSignature = req.headers.get('x-hub-signature');
-            if (!expectedSignature || req.method !== "POST" || !req.body) return new Response();
-            const signature = `sha1=${crypto.createHmac('sha1', config.gitSecret).update(await req.json()).digest('hex')}`;
+            if (!expectedSignature || req.method !== "POST" || !req.body) {
+                logHTTPRequest(404, req);
+                res.status = 404;
+                res.statusText = "Page Not Found";
+                return new Response(await generateErrorPage(404), res);
+            };
+            const signature = `sha1=${crypto.createHmac('sha1', config.gitSecret).update(await req.text()).digest('hex')}`;
 
             if (crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(expectedSignature))) {
                 exec('~/personal-website/.git/hooks/post-receive', async (error, stdout, stderr) => {
                     if (error) {
-                        logHTTPRequest(404, req);
-                        res.status = 404;
-                        res.statusText = "Language Not Found";
-                        return new Response(await generateErrorPage(404), res);
+                        console.log(stderr);
+                        logHTTPRequest(500, req);
+                        res.status = 500;
+                        res.statusText = "Error when executing the script";
+                        return new Response(await generateErrorPage(500), res);
                     }
                 });
+                return new Response("200", res);
             } else {
-                logHTTPRequest(404, req);
-                res.status = 404;
-                res.statusText = "Language Not Found";
-                return new Response(await generateErrorPage(404), res);
+                logHTTPRequest(418, req);
+                res.status = 404184;
+                res.statusText = "Crypto failed";
+                return new Response(await generateErrorPage(418), res);
             }
         }
         default: {
